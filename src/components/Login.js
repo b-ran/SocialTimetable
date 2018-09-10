92e240db9196a69fb0ebc238f163b61f760b4fc1
Brandon Scott-Hill <scotthbran@ecs.vuw.ac.nz>
2018-09-10 20:57:23 +1200

    Updated login to worked with common styles

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, Image, TextInput, Button, Alert, TouchableOpacity} from 'react-native';
-import {createStackNavigator} from "react-navigation";
+import {StyleSheet, Text, View, Image, TextInput, Button, Alert, TouchableOpacity} from 'react-native';
 import ForgottenPassword from "./ForgottenPassword";
+import {styles} from "../styles/common";
 
 export default class Login extends Component {
 
@@ -80,45 +80,3 @@ export default class Login extends Component {
         );
     }
 }
-
-
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#FF9800"
-    },
-    logo: {
-        width: 100,
-        height: 100
-
-    },
-    title: {
-        color: "#FFF",
-        marginTop: 10,
-        textAlign: "center",
-        width: 190
-    },
-    logoContainer: {
-        alignItems: "center",
-        flexGrow: 1,
-        justifyContent: "center"
-
-    },
-    formContainer: {
-        padding: 20
-    },
-    input: {
-      height: 40,
-      backgroundColor: "rgba(255,255,255,0.2)",
-      marginBottom: 20,
-      color: "#FFF"
-    },
-    subButtonsContainer: {
-        flexDirection: "row",
-        flexGrow: 1,
-        justifyContent: "center",
-        paddingTop: 10,
-    },
-
-});
