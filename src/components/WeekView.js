682cc317774705da646a777b4ee3b4514298537d
Brandon Scott-Hill <scotthbran@ecs.vuw.ac.nz>
2018-09-16 23:46:40 +1200

    Add week view page with native base components

---
+++ src/components/WeekView.js
@@ -0,0 +1,48 @@
+import {Container, Header, Left, Body, Right, Button, Icon, Title, Drawer} from "native-base"
+import React, {Component} from "react";
+import SideBar from "./Menu";
+import {styles} from "../styles/common";
+
+export default class WeekView extends Component {
+
+    static navigationOptions = {
+        header: null
+    };
+
+    closeDrawer() {
+        this._drawer._root.close();
+    };
+
+    openDrawer() {
+        this._drawer._root.open();
+    };
+
+    render() {
+        return (
+            <Drawer
+                ref={(ref) => {
+                    this._drawer = ref;
+                }}
+                content={<SideBar navigation={this.props.navigation} navigator={this._navigator}/>}
+                onClose={() => this.closeDrawer()}>
+
+                <Container>
+                    <Header style={styles.header}>
+                        <Left>
+                            <Button transparent onPress={() => this.openDrawer()}>
+                                <Icon name="menu"/>
+                            </Button>
+                        </Left>
+                        <Body>
+                        <Title>Week View</Title>
+                        </Body>
+                    </Header>
+                </Container>
+            </Drawer>
+        );
+    }
+
+
+}
+
+
