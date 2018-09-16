d40923cc40738bc97cd37e1a072f90f875ce0a7e
Brandon Scott-Hill <scotthbran@ecs.vuw.ac.nz>
2018-09-16 23:48:41 +1200

    Hide timer spam from firebase bug

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
 import App from "./App";
-import { YellowBox } from 'react-native';
+import {YellowBox} from 'react-native';
 //Fix to Bad Warning Message Credit: https://github.com/react-navigation/react-navigation/issues/3956
 YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader']);
+YellowBox.ignoreWarnings(['Setting a timer']);
 AppRegistry.registerComponent(appName, () => App);
