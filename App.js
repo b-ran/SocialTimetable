e883b1c222eb42cde85a78434995b38b5e538624
Brandon Scott-Hill <scotthbran@ecs.vuw.ac.nz>
2018-09-16 23:44:58 +1200

    Add firebase & native base

--- App.js
+++ App.js
@@ -1,23 +1,42 @@
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
 import {createStackNavigator} from "react-navigation";
 import Login from "./src/components/Login";
 import ForgottenPassword from "./src/components/ForgottenPassword";
 import Register from "./src/components/Register";
+import firebase from "firebase"
+import WeekView from "./src/components/WeekView";
+import Menu from "./src/components/Menu";
 
 type Props = {};
 export default class App extends Component<Props> {
-  render() {
-    return (
-        <StackNav />
-    );
-  }
+
+    componentWillMount() {
+
+        let config = {
+            apiKey: "AIzaSyCQMZQspYkphaxWubdOtY4QLgHjnnxTlNI",
+            authDomain: "timetable-6f3af.firebaseapp.com",
+            databaseURL: "https://timetable-6f3af.firebaseio.com",
+            projectId: "timetable-6f3af",
+            storageBucket: "timetable-6f3af.appspot.com",
+            messagingSenderId: "453223642924"
+        };
+        firebase.initializeApp(config);
+
+    }
+
+    render() {
+        return (
+            <StackNav/>
+        );
+    }
 }
 
 const StackNav = createStackNavigator({
+    WeekView: WeekView,
     Login: Login,
     ForgottenPassword: ForgottenPassword,
-    Register: Register
+    Register: Register,
+    Menu: Menu,
 });
 
 
