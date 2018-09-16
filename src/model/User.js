deb8dd5b3015d548d03d6dfda0b6d3302f959692
Brandon Scott-Hill <scotthbran@ecs.vuw.ac.nz>
2018-09-16 23:49:35 +1200

    Add user management logic to user

--- src/model/User.js
+++ src/model/User.js
@@ -2,7 +2,7 @@ import {AccountHandler} from "../external/AccountHandler";
 
 export class User {
 
-    state = {
+    static state = {
         uid: null,
         firstName: "",
         lastName: "",
@@ -10,7 +10,38 @@ export class User {
         password: "",
     };
 
-    static login(email, password) {
-        AccountHandler.attemptLogin(email, password);
+    static signIn(email, password) {
+        AccountHandler.attemptSignIn(email, password);
+    }
+
+    static signOut() {
+        AccountHandler.attemptSignOut();
+        state.clear();
+    }
+
+    static sendPasswordResetEmail(email) {
+        AccountHandler.attemptSendingPasswordResetEmail(email);
+    }
+
+    static register(firstName, lastName, email, password) {
+        this.state.firstName = firstName;
+        this.state.lastName = lastName;
+        this.state.email = email;
+        this.state.password = password;
+
+        AccountHandler.attemptRegistration(email, password);
+    }
+
+    static updateState(newUserState) {
+        this.state.uid = newUserState.uid;
+        this.state.firstName = newUserState.firstName;
+        this.state.lastName = newUserState.lastName;
+        this.state.email = newUserState.email;
+        this.state.password = newUserState.password;
+
+    }
+
+    static online() {
+        return !this.state.isEmpty();
     }
 }
