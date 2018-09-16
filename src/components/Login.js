c3489ed442679df3c798fdc914ad63601c986385
Brandon Scott-Hill <scotthbran@ecs.vuw.ac.nz>
2018-09-16 23:48:00 +1200

    Add sign in logic to login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,18 +1,16 @@
 import React, {Component} from 'react';
 import {StyleSheet, Text, View, Image, TextInput, Button, Alert, TouchableOpacity} from 'react-native';
-import ForgottenPassword from "./ForgottenPassword";
 import {styles} from "../styles/common";
 import {User} from "../model/User";
 
 export default class Login extends Component {
 
     static navigationOptions = {
-        header: null,
-        // title: "Login",
-        // headerStyle: {
-        //     backgroundColor: "#FFB039",
-        // },
-        // headerTintColor: '#FFF',
+        title: "Login",
+        headerStyle: {
+            backgroundColor: "#FFB039",
+        },
+        headerTintColor: '#FFF',
     };
 
     state = {
@@ -20,63 +18,70 @@ export default class Login extends Component {
         password: "",
     };
 
-    login = () => {
-        const { email, password } = this.state;
-        User.login(email, password);
+    signIn = () => {
+        console.log("signIn");
+        const {email, password} = this.state;
+        User.signIn(email, password);
+        this.props.navigation.navigate("WeekView");
     };
 
     render() {
         return (
-          <View style={styles.container}>
+            <View style={styles.container}>
 
-              <View style={styles.logoContainer}>
-                  <Image style={styles.logo} source={require("../../assets/timetable-logo.png")}/>
-                  <Text style={styles.title}>The Social Timetable App For You and Your Friends</Text>
-              </View>
+                <View style={styles.logoContainer}>
+                    <Image style={styles.logo} source={require("../../assets/timetable-logo.png")}/>
+                    <Text style={styles.title}>The Social Timetable App For You and Your Friends</Text>
+                </View>
 
-              <View style={styles.formContainer}>
+                <View style={styles.formContainer}>
 
-                  <TextInput
-                      onChangeText={(value) => this.setState({email: value})}
-                      returnKeyType = {"next"}
-                      placeholder={"Email"}
-                      placeholderTextColor={"white"}
-                      keyboardType={"email-address"}
-                      autoCapitalize={"none"}
-                      autoCorrect={false}
-                      style={styles.input}
-                      onSubmitEditing={() => { this.passwordTextInput.focus(); }}
-                      blurOnSubmit={false}
-                      underlineColorAndroid={"rgba(0,0,0,0)"}
-                  />
-                  <TextInput
-                      onChangeText={(value) => this.setState({password: value})}
-                      placeholder={"Password"}
-                      secureTextEntry={true}
-                      placeholderTextColor={"white"}
-                      autoCapitalize={"none"}
-                      autoCorrect={false}
-                      style={styles.input}
-                      ref={(input) => { this.passwordTextInput = input; }}
-                      onSubmitEditing={this.login}
-                      underlineColorAndroid={"rgba(0,0,0,0)"}
-                  />
+                    <TextInput
+                        onChangeText={(value) => this.setState({email: value})}
+                        returnKeyType={"next"}
+                        placeholder={"Email"}
+                        placeholderTextColor={"white"}
+                        keyboardType={"email-address"}
+                        autoCapitalize={"none"}
+                        autoCorrect={false}
+                        style={styles.input}
+                        onSubmitEditing={() => {
+                            this.passwordTextInput.focus();
+                        }}
+                        blurOnSubmit={false}
+                        underlineColorAndroid={"rgba(0,0,0,0)"}
+                    />
+                    <TextInput
+                        onChangeText={(value) => this.setState({password: value})}
+                        placeholder={"Password"}
+                        secureTextEntry={true}
+                        placeholderTextColor={"white"}
+                        autoCapitalize={"none"}
+                        autoCorrect={false}
+                        style={styles.input}
+                        ref={(input) => {
+                            this.passwordTextInput = input;
+                        }}
+                        onSubmitEditing={this.signIn}
+                        underlineColorAndroid={"rgba(0,0,0,0)"}
+                    />
 
-                  <Button title={"Login"} onPress={this.login} color={"#FFB039"}/>
+                    <Button title={"Login"} onPress={this.signIn} color={"#FFB039"}/>
 
-                  <View style={styles.subButtonsContainer}>
-                    <TouchableOpacity onPress={()=> this.props.navigation.navigate("ForgottenPassword")} style={{marginRight: 10}}>
-                        <Text style={{color:"#FFF"}}>Forgotten Password</Text>
-                    </TouchableOpacity>
+                    <View style={styles.subButtonsContainer}>
+                        <TouchableOpacity onPress={() => this.props.navigation.navigate("ForgottenPassword")}
+                                          style={{marginRight: 10}}>
+                            <Text style={{color: "#FFF"}}>Forgotten Password</Text>
+                        </TouchableOpacity>
 
-                    <TouchableOpacity onPress={()=> this.props.navigation.navigate("Register")} >
-                        <Text style={{color:"#FFF"}}>Register</Text>
-                    </TouchableOpacity>
-                  </View>
+                        <TouchableOpacity onPress={() => this.props.navigation.navigate("Register")}>
+                            <Text style={{color: "#FFF"}}>Register</Text>
+                        </TouchableOpacity>
+                    </View>
 
-              </View>
+                </View>
 
-          </View>
+            </View>
 
         );
     }
