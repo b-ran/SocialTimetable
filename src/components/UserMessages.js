179e1a6c2fd46e39dc906db47281ad2ad97a514e
Brandon Scott-Hill <scotthbran@ecs.vuw.ac.nz>
2018-09-18 00:01:27 +1200

    Add toasting system for userfeedback

--- src/components/UserMessages.js
+++ src/components/UserMessages.js
@@ -1,6 +1,23 @@
+import {Toast} from "native-base";
+
 export class UserMessages {
 
     static MISSING_EMAIL: string = "Please Input Email Address";
     static MISSING_DATA: string = "Please Fill in all Fields";
+    static SIGN_OUT: string = "Signed Out!";
+
+    static sign_out_toast() {
+        this.toastr.showToast(this.SIGN_OUT);
+    }
+
+    static toastr = {
+        showToast: (message, duration = 2500) => {
+            Toast.show({
+                text: message,
+                duration,
+                buttonText: 'Okay',
+            });
+        },
+    };
 
 }
