4e5f219c13757fc2e0fa017a45c987520c5bf736
Brandon Scott-Hill <scotthbran@ecs.vuw.ac.nz>
2018-09-18 00:02:48 +1200

    Menu updates it appears on screen & Move button section to subcomponent for better state control

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,14 +1,35 @@
 import React, {Component} from "react";
 import {StyleSheet} from "react-native";
 import {Content, Button, Icon, Text} from "native-base"
+import {User} from "../model/User"
+import {UserMessages} from "./UserMessages"
 
 
 export default class Menu extends Component {
 
-    render() {
-        return(
-            <Content style={styles.content}>
+    count = 0;
 
+    componentDidMount() {
+        this.props.navigation.addListener(
+            "willFocus",
+            payload => {
+                this.forceUpdate();
+                console.log("didFocus");
+            }
+        )
+
+    }
+
+    signOut() {
+        User.signOut();
+        UserMessages.sign_out_toast();
+        this.forceUpdate();
+    }
+
+    friendButtons(isOnline) {
+        if (isOnline) {
+            return (
+                <Content>
                     <Button transparent iconLeft>
                         <Icon style={styles.button} name='md-person-add'/>
                         <Text style={styles.button}>Add Friends</Text>
@@ -18,23 +39,52 @@ export default class Menu extends Component {
                         <Icon style={styles.button} name='md-people'/>
                         <Text style={styles.button}>All Friends</Text>
                     </Button>
+                </Content>
+            );
+        }
+    }
 
-                    <Button transparent iconLeft>
-                        <Icon style={styles.button} name='md-information-circle'/>
-                        <Text style={styles.button}>About</Text>
-                    </Button>
-                    <Button transparent iconLeft onPress={() => this.props.navigation.navigate("Login")}>
-                        <Icon style={styles.button} name='md-log-in'/>
-                        <Text style={styles.button}>Account</Text>
-                    </Button>
-                    <Button transparent iconLeft>
-                        <Icon style={styles.button} name='cog' />
-                        <Text style={styles.button}>Settings</Text>
-                    </Button>
-                    <Button transparent iconLeft>
-                        <Icon style={styles.button} name='md-log-out'/>
-                        <Text style={styles.button}>Sign Out</Text>
-                    </Button>
+    test() {
+        this.count++;
+        return (<Text>{this.count}</Text>);
+    }
+
+    accountButton(isOnline) {
+        if (isOnline) {
+            return (
+                <Button transparent iconLeft onPress={() => this.signOut()}>
+                    <Icon style={styles.button} name='md-log-out'/>
+                    <Text style={styles.button}>Sign Out</Text>
+                </Button>
+            );
+        } else {
+            return (
+                <Button transparent iconLeft onPress={() => this.props.navigation.navigate("Login")}>
+                    <Icon style={styles.button} name='md-log-in'/>
+                    <Text style={styles.button}>Account</Text>
+                </Button>
+            );
+        }
+    }
+
+    render() {
+        return (
+            <Content style={styles.content}>
+
+                {this.test()}
+                {this.friendButtons(true)}
+
+                <Button transparent iconLeft>
+                    <Icon style={styles.button} name='md-information-circle'/>
+                    <Text style={styles.button}>About</Text>
+                </Button>
+                <Button transparent iconLeft>
+                    <Icon style={styles.button} name='cog'/>
+                    <Text style={styles.button}>Settings</Text>
+                </Button>
+
+                {this.accountButton(User.isOnline())}
+
             </Content>
         );
     }
