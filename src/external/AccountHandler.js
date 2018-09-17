9efa35e8fa0225b1f2e910431f68ed297f6dab2a
Brandon Scott-Hill <scotthbran@ecs.vuw.ac.nz>
2018-09-18 00:00:34 +1200

    Updated sign in to return a promise for better control

--- src/external/AccountHandler.js
+++ src/external/AccountHandler.js
@@ -1,28 +1,31 @@
-//import reactFirebase from "react-native-firebase";
-//import * as firebase from "react-native-firebase";
 import {User} from "../model/User";
 import firebase from "firebase"
 
 export class AccountHandler {
 
 
-    static async attemptSignIn(email, password) {
-        if (email == null || email === "" || password == null || password === "") {
-            throw new Error(UserMessages.MISSING_DATA);
-        }
-        await firebase.auth().signInWithEmailAndPassword(email, password);
-        firebase.database().ref("user/" + firebase.auth().currentUser.uid).on("value", (data)=> {
-            let rawUserData = data.toJSON();
-            let newUserState = {
-                uid: firebase.auth().currentUser.uid,
-                firstName: rawUserData.firstName,
-                lastName: rawUserData.lastName,
-                email: email,
-                password: password,
-            };
-            User.updateState(newUserState);
-            console.log("Updated Logged User");
+    static attemptSignIn(email, password) {
+        return new Promise( (resolve) => {
+            if (email == null || email === "" || password == null || password === "") {
+                throw new Error(UserMessages.MISSING_DATA);
+            }
+            firebase.auth().signInWithEmailAndPassword(email, password).then(() => {
+                firebase.database().ref("user/" + firebase.auth().currentUser.uid).once("value", (data)=> {
+                    let rawUserData = data.toJSON();
+                    let newUserState = {
+                        uid: firebase.auth().currentUser.uid,
+                        firstName: rawUserData.firstName,
+                        lastName: rawUserData.lastName,
+                        email: email,
+                        password: password,
+                    };
+                    User.updateState(newUserState);
+                    console.log("Updated Logged User");
+                    resolve();
+                });
+            });
         });
+
     }
 
     static async attemptSignOut() {
