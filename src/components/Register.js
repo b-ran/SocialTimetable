6dfa7a06c6a1fbd6a123f7a15863591a341ca2ee
Brandon Scott-Hill <scotthbran@ecs.vuw.ac.nz>
2018-09-17 23:58:32 +1200

    Add native-base header

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,17 +1,12 @@
 import React, {Component} from 'react';
-import {Text, View, Image, TextInput, Button, Alert, TouchableOpacity, ScrollView} from 'react-native';
-import {styles} from "../styles/common";
+import {Text, View, Image, TextInput, Alert, TouchableOpacity, ScrollView} from 'react-native';
+import {Header, Left, Body, Right, Icon, Button, Title} from "native-base"
+import {navigationOptions, styles} from "../styles/common";
 import {User} from "../model/User";
 
 export default class Register extends Component {
 
-    static navigationOptions = {
-        title: "Register",
-        headerStyle: {
-            backgroundColor: "#FFB039",
-        },
-        headerTintColor: '#FFF',
-    };
+    static navigationOptions = navigationOptions;
 
     state = {
         firstName: "",
@@ -24,6 +19,18 @@ export default class Register extends Component {
     render() {
         return (
             <ScrollView resetScrollToCoords={{x: 0, y: 0}} contentContainerStyle={styles.container}>
+
+                <Header style={styles.header}>
+                    <Left>
+                        <Button transparent onPress={() => this.props.navigation.goBack()}>
+                            <Icon name="arrow-back"/>
+                        </Button>
+                    </Left>
+                    <Body>
+                    <Title>Register</Title>
+                    </Body>
+                </Header>
+
                 <View style={styles.logoContainer}>
                     <Image style={styles.logo} source={require("../../assets/timetable-logo.png")}/>
                     <Text style={styles.title}>The Social Timetable App For You and Your Friends</Text>
