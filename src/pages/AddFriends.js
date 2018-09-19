5a112d647fb740ef05f4c0465d5f6a07e1fc26ec
Brandon Scott-Hill <scotthbran@ecs.vuw.ac.nz>
2018-09-20 00:04:23 +1200

    Fixed looping search bug & Made users into buttons to request

--- src/pages/AddFriends.js
+++ src/pages/AddFriends.js
@@ -1,20 +1,17 @@
 import React, {Component} from "react";
-import {Button, Container, Header, Icon, Input, Item, Left, Text, List, ListItem} from "native-base";
+import {Button, Container, Header, Icon, Input, Item, Left, Right, Text, List, ListItem} from "native-base";
 
 import {navigationOptions, styles} from "../styles/common";
 import {OtherUsers} from "../model/OtherUsers";
+import {Requests} from "../model/Requests";
 
 export default class AddFriends extends Component {
 
     static navigationOptions = navigationOptions;
 
-    state = {
-        search: "",
-    };
-
     render() {
-        return(
-            <Container style={styles.container}>
+        return (
+            <Container>
                 <Header searchBar rounded style={styles.header}>
                     <Left>
                         <Button transparent onPress={() => this.props.navigation.goBack()}>
@@ -23,7 +20,7 @@ export default class AddFriends extends Component {
                     </Left>
                     <Item style={{flex: 3}}>
                         <Icon name="search"/>
-                        <Input onChangeText={(value) => this.setState({search: value})} placeholder="Search Users"/>
+                        <Input onChangeText={(value) => this.input(value)} placeholder="Search Users"/>
                         <Icon name="people"/>
                     </Item>
                 </Header>
@@ -35,21 +32,32 @@ export default class AddFriends extends Component {
     }
 
     drawUsers() {
-        return(
+        return (
             <List dataArray={OtherUsers.users}
                   renderRow={(item) =>
-                      <ListItem>
-                          <Text>{item.firstName + " " + item.lastName}</Text>
+                      <ListItem button onPress={() => Requests.makeRequest(item)}>
+                          <Left>
+                              <Text style={styles.subtext}># {item.uid.substring(0, 5)} </Text>
+                              <Text style={styles.text}>{item.firstName + " " + item.lastName}</Text>
+                          </Left>
+                          <Right>
+                              <Icon name="add"/>
+                          </Right>
                       </ListItem>
-                  }>
+                  }
+                  renderHeader={() =>
+                      <ListItem itemHeader first>
+                          <Text>Users</Text>
+                      </ListItem>
+                  }
+            >
+
             </List>
         );
     }
 
-    componentDidUpdate() {
-        //console.log(this.state.search);
-        OtherUsers.findUsers(this.state.search).then(() => {
-            console.log(OtherUsers.users);
+    input(value) {
+        OtherUsers.findUsers(value).then(() => {
             this.forceUpdate();
         });
     }
