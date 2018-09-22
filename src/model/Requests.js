d5f5dd945a40e13c907b66117080f397285bda6b
Brandon Scott-Hill <scotthbran@ecs.vuw.ac.nz>
2018-09-22 13:47:04 +1200

    Add get all request data for current user & Add accepting and making requests

--- src/model/Requests.js
+++ src/model/Requests.js
@@ -1,10 +1,43 @@
 import {FriendHandler} from "../external/FriendHandler";
+import {UsersHandler} from "../external/UsersHandler";
+import {OtherUsers} from "./OtherUsers";
+import AddFriends from "../pages/AddFriends";
+import {Requested} from "./Requested";
+import {Friends} from "./Friends";
 
 export class Requests {
 
+    static data = [];
 
     static makeRequest(targetUser) {
-        FriendHandler.request(targetUser);
+        return new Promise(async (resolve) => {
+            await FriendHandler.request(targetUser);
+            await Requested.populateRequested();
+            await OtherUsers.filterUsers();
+            resolve();
+        });
+    }
+
+    static acceptRequest(targetUser) {
+        return new Promise(async (resolve) => {
+            await FriendHandler.acceptRequest(targetUser);
+            await this.populateRequests();
+            await OtherUsers.filterUsers();
+            resolve();
+        });
+    }
+
+    static populateRequests() {
+        return new Promise((resolve) => {
+            UsersHandler.attemptToGetRequests().then(()=> {
+                this.data = [];
+                this.data = UsersHandler.users;
+                console.log("------Requests------");
+                console.log(this.data);
+            });
+            resolve();
+        });
+
 
     }
 
