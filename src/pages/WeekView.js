678321636718497c4dfce664456d870a8a24b199
Brandon Scott-Hill <scotthbran@ecs.vuw.ac.nz>
2018-09-24 00:01:26 +1200

    Made drawer open account page when not logged in

--- src/pages/WeekView.js
+++ src/pages/WeekView.js
@@ -1,24 +1,30 @@
-import {Container, Header, Left, Body, Right, Button, Icon, Title, Drawer} from "native-base"
+import {Container, Drawer, Content, Fab, View, Icon} from "native-base"
 import React, {Component} from "react";
 import SideBar from "../components/Menu";
 import {styles, navigationOptions} from "../styles/common";
 import {createHeaderButton} from "../components/Header";
+import {User} from "../model/User";
 
 export default class WeekView extends Component {
 
     static navigationOptions = navigationOptions;
 
     componentDidMount() {
-
+        this.props.navigation.addListener(
+            "willFocus",
+            payload => {
+                this.forceUpdate();
+            }
+        )
     }
 
     closeDrawer() {
         this._drawer._root.close();
-    };
+    }
 
     openDrawer() {
         this._drawer._root.open();
-    };
+    }
 
     render() {
         return (
@@ -26,18 +32,43 @@ export default class WeekView extends Component {
                 ref={(ref) => {
                     this._drawer = ref;
                 }}
-                content={<SideBar navigation={this.props.navigation} navigator={this._navigator}/>}
+                content={<SideBar navigation={this.props.navigation} navigator={this.navigator} closeDrawer={()=> this.closeDrawer()}/>}
                 onClose={() => this.closeDrawer()}>
 
                 <Container>
-                    {createHeaderButton("Week View", () => this.openDrawer(), "menu")}
+                    {this.statusMenu()}
                 </Container>
 
+                    <Fab
+                        active={true}
+                        direction="up"
+                        containerStyle={{}}
+                        style={styles.fab}
+                        position="bottomRight"
+                        onPress={() => this.props.navigation.navigate("AddLesson")}>
+                        <Icon name="add" />
+                    </Fab>
 
 
             </Drawer>
         );
     }
+
+    statusMenu() {
+        if (User.isOnline()) {
+            return(
+              <Content>
+                  {createHeaderButton("Week View", () => this.openDrawer(), "menu")}
+              </Content>
+            );
+        }
+        return(
+            <Content>
+                {createHeaderButton("Week View", () => this.props.navigation.navigate("Login"), "menu")}
+            </Content>
+        );
+    }
+
 
 
 }
