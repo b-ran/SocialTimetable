bad343a49c3670c40d1e625f3a26219f20e415bf
Brandon Scott-Hill <scotthbran@ecs.vuw.ac.nz>
2018-09-30 01:00:30 +1200

    Got drawing lessons on timetable with correct size going

--- src/components/Timetable.js
+++ src/components/Timetable.js
@@ -1,29 +1,31 @@
 import React, {Component} from "react";
 import {Container, Icon, Left, List, ListItem, Text, Right, Col, Row, Grid, Content} from "native-base";
-import {StyleSheet} from "react-native";
+import {StyleSheet, View} from "react-native";
+import type {Lesson} from "../model/Lesson";
+
+
+
+
 
 export default class Timetable extends Component  {
 
     props: {
-        //weekDays: string[],
-        //lessons: [],
+        lessons: [],
         starHour: number,
         endHour: number,
     };
 
-
     state = {
         times: [],
-        weekDays: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+        weekDays: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
     };
+
+    viewHeight = 600;
 
     render() {
         this.populateTimes();
         const {
-            //weekDays,
-            //lessons,
-            starHour,
-            endHour,
+            lessons,
         } = this.props;
         const {
             times,
@@ -40,11 +42,17 @@ export default class Timetable extends Component  {
                                 </Row>
                             ))}
                         </Col>
-                        {weekDays.map((item, key)=>(
-                            <Col>
+                        {weekDays.map((day, key)=>(
+                            <Col onLayout={(event)=> {this.setViewSize(event.nativeEvent.layout)}}>
                                 <Row>
-                                    <Text key={key}>{item}</Text>
+                                    <Text key={key} style={{left: "1.5%"}}>{day.substring(0,2)}</Text>
                                 </Row>
+                                {this.getLessonsOnDay(day, lessons).map((lesson, key)=> (
+                                    <View style={this.getLessonStyle(lesson)}>
+                                        <Text key={key} style={{color: "white"}}>{lesson.abbreviation}</Text>
+                                    </View>
+
+                                ))}
                             </Col>
                         ))}
                     </Grid>
@@ -60,9 +68,49 @@ export default class Timetable extends Component  {
         for (let i = this.props.starHour; i < this.props.endHour+1; i++) {
             this.state.times.push(i);
         }
-        console.log(this.state.times);
     }
 
+    getLessonStyle(lesson) {
+        const {
+            starHour,
+            endHour,
+        } = this.props;
+        let range = endHour - starHour;
+        let lessonHourRange: number = parseInt(lesson.endTime) - parseInt(lesson.startTime);
+        let lessonHeight = (this.viewHeight / range) * lessonHourRange;
+        let position = this.viewHeight-((this.viewHeight / range) * (lesson.endTime-starHour));
+        //console.log(lesson.abbreviation, position, this.viewHeight);
+
+        let styles = StyleSheet.create({
+            row: {
+                backgroundColor: lesson.color,
+                height: lessonHeight,
+                bottom: position-25,
+                width: "90%",
+                position:'absolute',
+                alignSelf:'flex-end',
+            }
+        });
+        return styles.row;
+    }
+
+    getLessonsOnDay(day: string, lessons: []) {
+        let lessonsOnDay: [] = [];
+        if (lessons.length >= 0) {
+            for(let i = 0; i < lessons.length; i++) {
+                if (lessons[i].day === day) {
+                    lessonsOnDay.push(lessons[i]);
+                }
+            }
+        }
+        lessonsOnDay.sort((a,b) => {return b.endTime - a.endTime});
+        return lessonsOnDay;
+    }
+
+    setViewSize(layout) {
+        const {x, y, width, height} = layout;
+        this.viewHeight = height;
+    }
 }
 
 const styles = StyleSheet.create({
@@ -74,7 +122,7 @@ const styles = StyleSheet.create({
         height: 600,
     },
     timeCol: {
-        top: "5%",
+        top: "10%",
         left: "2.5%",
         height: 500,
     },
